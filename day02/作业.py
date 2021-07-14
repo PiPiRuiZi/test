4f481aebf9042268a61b51bfe08796e768ec6968
zhaorui <rui.999@foxmail.com>
2021-07-14 15:58:17 +0800

    day02的作业

---
+++ day02/作业.py
@@ -0,0 +1,36 @@
+print("1.有以下两个数，使用+，-号实现两个数的调换")
+a = 56
+b = 78
+
+print("交换前")
+print("a =", a, ", b =", b)
+
+a = a + b
+b = a - b
+a = a - b
+
+print("交换后")
+print("a =", a, ", b =", b)
+print("-"*70)
+
+print("2.定义两个变量stu1和stu2，分别存储45和23，并用print()打印两个数的和")
+stu1 = 45
+stu2 = 23
+print("总和为：", stu1+stu2)
+print("-"*70)
+
+print("3.定义5个变量，并用print()打印5个数的和、平均值")
+a = 55
+b = 66
+c = 77
+d = 88
+e = 99
+print("总和为：", a+b+c+d+e)
+print("平均值为：", (a+b+c+d+e)/5)
+print("-"*70)
+
+print("4.定义一个变量num1,并赋值45.然后将num1值赋值给num2。打印出来")
+num1 = 45
+num2 = num1
+print("num1 =", num1)
+print("num2 =", num2)
