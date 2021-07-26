bf3306be9071f828bac46ddccc91380bfadcdb9d
zhaorui <rui.999@foxmail.com>
2021-07-26 20:05:15 +0800

    day08的作业

---
+++ day08/基于Mysql银行系统实现.py
@@ -0,0 +1,355 @@
+# -*- coding:utf-8 -*-
+import random  # 随机数
+import os  # 按任意键继续
+import pymysql
+
+"""
+    账户ID为键
+"""
+
+# 开户行名称
+bank_name = "中国建设银行昌平支行"
+# 欢迎页面模板
+wellCome = """
+        ------------------------------------
+        |      欢迎来到中国建设银行昌平支行    |
+        ------------------------------------
+        |             1.开户               |
+        |             2.存钱               |
+        |             3.取钱               |
+        |             4.转账               |
+        |             5.查询               |
+        |             6.退出               |
+        ------------------------------------
+"""
+
+
+# 自动生成账户id,不能和已有的重复
+def create_account(data, len):
+    account = random.randint(10000000, 99999999)
+
+    for i in range(len):
+        if account == data[i][0]:
+            account = create_account(data, len)  # 如果已存在,则递归
+
+    return account
+
+
+# 银行添加用户
+def bank_add_user(username, password, money, country, province, street, door):
+    # 创建连接
+    con = pymysql.connect(host="localhost", user="root", passwd="111111", db="bank")
+    # 创建控制台
+    cur = con.cursor()
+
+    sql = "select id from user"
+    cur.execute(sql)
+    data = cur.fetchall()
+    length = len(data)
+
+    # 用户库已满
+    if length >= 100:
+        return False
+
+    account = create_account(data, length)
+
+    # 录入
+    global bank_name
+    sql = "insert into user values(%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    param = [account, username, password, money, country, province, street, door, bank_name]
+    cur.execute(sql, param)
+    # 提交数据到数据库
+    con.commit()
+    # 关闭资源
+    cur.close()
+    con.close()
+    return account
+
+
+# 开户
+def add_user():
+    global bank_name
+    username = input("请输入账户用户名：")
+    password = input("请输入账户密码：")
+    money = input("请输入账户金额：")
+    if money.isdigit():
+        money = int(money)
+        country = input("请输入所属国籍：")
+        province = input("请输入所属省份：")
+        street = input("请输入所属街道：")
+        door = input("请输入门牌号：")
+        account: int
+        status = bank_add_user(username, password, money, country, province, street, door)
+        if status is False:
+            print("用户库存已达上限！")
+        else:
+            account = status
+            con = pymysql.connect(host="localhost", user="root", passwd="111111", db="bank")
+            cur = con.cursor()
+            sql = "select * from user where id = {0}".format(account)
+            cur.execute(sql)
+            print("开户成功！")
+            info = """
+                ----------个人信息【建设银行】---------
+                账户ID：{account}
+                用户名：{username}
+                密码：{password}
+                金额：{money}
+                居住地址：
+                    国籍：{country}
+                    省份：{province}
+                    街道：{street}
+                    门牌号：{door}
+                开户行地址：{bank_name_tmp}
+                ------------------------------------
+            """
+            print(info.format(account=account, username=username, password=password,
+                              money=money, country=country, province=province,
+                              street=street, door=door, bank_name_tmp=bank_name))
+
+    else:
+        print("输入不合法！请重新输入")
+
+
+# 存钱
+def bank_add_money(account, add_money):
+    con = pymysql.connect(host="localhost", user="root", passwd="111111", db="bank")
+    cur = con.cursor()
+
+    sql = "select id from user"
+    cur.execute(sql)
+    data = cur.fetchall()
+    length = len(data)
+
+    tmp = False
+    for i in range(length):
+        if account == data[i][0]:
+            tmp = True
+            break
+    if tmp:
+        sql = "update user set money = money + {0} where id = {1}".format(add_money, account)
+        cur.execute(sql)
+        # 提交数据到数据库
+        con.commit()
+        return True
+    else:
+        return False
+
+
+def add_money():
+    while True:
+        account = input("请输入您的账号：")
+        if account.isdigit():
+            account = int(account)
+            while True:
+                money = input("请输入您的存款金额：")  # 将输入金额转换成int类型
+                if money.isdigit():
+                    money = int(money)
+                    status = bank_add_money(account, money)
+                    if status:
+                        con = pymysql.connect(host="localhost", user="root", passwd="111111", db="bank")
+                        cur = con.cursor()
+                        sql = "select money from user where id = {0}".format(account)
+                        cur.execute(sql)
+                        data = cur.fetchone()
+                        con.commit()
+                        con.close()
+                        cur.close()
+                        print("恭喜您存款成功！")
+                        print("当前余额为：", data[0])
+                        return
+                    else:
+                        print("未找到该用户，存款失败！请重新输入！")
+                        add_money()
+                        return
+                else:
+                    print("输入不合法！请重新输入")
+        else:
+            print("输入不合法！请重新输入")
+
+
+# 取钱
+def bank_take_money(account1, password1, money1):
+    con = pymysql.connect(host="localhost", user="root", passwd="111111", db="bank")
+    cur = con.cursor()
+    sql = "select id,password,money from user where id = {0}".format(account1)
+    cur.execute(sql)
+    data = cur.fetchone()
+    if data != None and account1 == data[0]:
+        if password1 == data[1]:
+            if money1 > data[2]:
+                return 2  # 余额不够
+            else:
+                sql = "update user set money = money - {0} where id = {1}".format(money1, account1)
+                cur.execute(sql)
+                # 提交数据到数据库
+                con.commit()
+                con.close()
+                cur.close()
+                return 3  # 成功取钱
+        else:
+            return 1  # 密码不正确
+    else:
+        return 0  # 账号不存在
+
+
+def take_money():
+    while True:
+        account1 = input("请输入您的账号：\n")
+        if account1.isdigit():
+            account1 = int(account1)
+            password1 = input("请输入密码：\n")
+            while True:
+                money1 = input('请输入取款金额：\n')
+                if money1.isdigit():
+                    money1 = int(money1)
+                    data = bank_take_money(account1, password1, money1)
+                    if data == 3:
+                        print("恭喜您取款成功！")
+                        con = pymysql.connect(host="localhost", user="root", passwd="111111", db="bank")
+                        cur = con.cursor()
+                        sql = "select money from user where id = {0}".format(account1)
+                        cur.execute(sql)
+                        data = cur.fetchone()
+                        con.commit()
+                        con.close()
+                        cur.close()
+                        print("当前余额为：", data[0])
+                        return
+                    elif data == 2:
+                        print("您的余额不足！")
+                    elif data == 1:
+                        print("您输入的密码不正确！")
+                    else:
+                        print("您输入的账号有误，请重新输入！")
+                else:
+                    print("输入不合法！请重新输入")
+        else:
+            print("输入不合法！请重新输入")
+
+
+# 转账
+def transfer():
+    con = pymysql.connect(host="localhost", user="root", passwd="111111", db="bank")
+    cur = con.cursor()
+    while True:
+        account = input("请输入您的账户ID：")
+        if account.isdigit():
+            account = int(account)
+            sql = "select id, password, money from user where id = {0}".format(account)
+            cur.execute(sql)
+            con.commit()
+            data = cur.fetchone()
+            if account == data[0]:
+                while True:
+                    password = input("请输入您的密码：")
+                    if password == data[1]:
+                        print("登录成功！您当前余额为：", data[2])
+                        while True:
+                            payee = input("请输入收款人账户ID：")
+                            if payee.isdigit():
+                                payee = int(payee)
+                                if payee == account:  # 收款人不能是自己
+                                    print("收款人不能是自己！请重新输入")
+                                else:
+                                    sql = "select id, password, money from user where id = {0}".format(payee)
+                                    cur.execute(sql)
+                                    con.commit()
+                                    data1 = cur.fetchone()
+                                    if data1 != None and payee == data1[0]:  # 判断收款人是否存在
+                                        while True:
+                                            s = input("请输入转账金额：")
+                                            if s.isdigit():
+                                                s = int(s)
+                                                if s > data[2]:  # 判断余额是否充足
+                                                    print("余额不足")
+                                                else:
+                                                    sql = "update user set money = money - {0} where id = {1}".format(s, account)
+                                                    cur.execute(sql)
+                                                    sql = "select money from user where id = {0}".format(account)
+                                                    cur.execute(sql)
+                                                    con.commit()
+                                                    tmp = cur.fetchone()
+                                                    print("转帐成功！您当前余额为：", tmp[0])
+                                                    sql = "update user set money = money + {0} where id = {1}".format(s, payee)
+                                                    cur.execute(sql)
+                                                    con.commit()
+                                                    con.close()
+                                                    cur.close()
+                                                    return
+                                            else:
+                                                print("输入不合法！请重新输入")
+                                    else:
+                                        print("您输入的收款人不存在！请重新输入")
+                            else:
+                                print("输入不合法！请重新输入")
+                    else:
+                        print("您输入的密码有误！请重新输入！")
+            else:
+                con.close()
+                cur.close()
+                print("您输入的账户不存在，请重新输入！")
+                transfer()
+        else:
+            print("输入不合法！请重新输入")
+
+# 查询
+def inquiry():
+    while True:
+        account = input("输入您的账户：")
+        if account.isdigit():
+            account = int(account)
+            con = pymysql.connect(host="localhost", user="root", passwd="111111", db="bank")
+            cur = con.cursor()
+            sql = "select * from user where id = {0}".format(account)
+            cur.execute(sql)
+            con.commit()
+            data = cur.fetchone()
+            if data != None and account == data[0]:
+                while True:
+                    password = input("输入您的密码：")
+                    if password != data[2]:  # 判断密码是否正确
+                        print("您输入的密码错误，请重新输入！")
+                    else:
+                        print("登陆成功，以下是您的个人信息")
+                        info = """
+        -----------个人信息---------
+        用户ID：%s
+        用户名：%s
+        密码：%s
+        地址信息
+            国家：%s
+            省份：%s
+            街道：%s
+            门牌号: %s
+        余额：%s
+        开户行地址：%s
+        ---------------------------
+                        """
+                        print(info % (data[0], data[1], data[2],data[4], data[5], data[6], data[7], data[3], data[8]))
+                        return
+            else:
+                print("您输入的账户不存在，请重新输入！")
+        else:
+            print("输入不合法！请重新输入")
+
+
+# 入口程序
+while True:
+    print(wellCome)
+    choose = input("请输入需求业务编号：")
+    if choose == '1':
+        add_user()  # 开户
+    elif choose == '2':
+        add_money()  # 存钱
+    elif choose == '3':
+        take_money()  # 取钱
+    elif choose == '4':
+        transfer()  # 转账
+    elif choose == '5':
+        inquiry()  # 查询
+    elif choose == '6':
+        break  # 退出
+    else:
+        print("输入非法！请重新输入")
+    os.system('pause')  # 按任意键继续
---
+++ day08/银行系统的数据库.sql
@@ -0,0 +1,43 @@
+/*
+SQLyog Ultimate v11.33 (64 bit)
+MySQL - 5.6.24 : Database - bank
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`bank` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `bank`;
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(20) DEFAULT NULL,
+  `password` varchar(20) DEFAULT NULL,
+  `money` int(10) unsigned DEFAULT NULL,
+  `country` varchar(20) DEFAULT NULL,
+  `province` varchar(20) DEFAULT NULL,
+  `street` varchar(20) DEFAULT NULL,
+  `door` varchar(20) DEFAULT NULL,
+  `bank_name` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `user` */
+
+insert  into `user`(`id`,`username`,`password`,`money`,`country`,`province`,`street`,`door`,`bank_name`) values (10000001,'test1','123456',9394,'中国','北京','沙河镇街道','s001','中国建设银行昌平支行'),(10000002,'test2','654321',20611,'中国','北京','回龙观街道','s002','中国建设银行昌平支行'),(10000003,'test3','123456',30000,'中国','北京','中关村街道','s003','中国建设银行昌平支行'),(45214284,'1','1',1,'1','1','1','1','中国建设银行昌平支行'),(48787682,'zhaorui','111111',200,'中国','北京','天安门','003','中国建设银行昌平支行'),(82763832,'李白','123456',1230,'中国','河北','实际安装','的说法','中国建设银行昌平支行');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
