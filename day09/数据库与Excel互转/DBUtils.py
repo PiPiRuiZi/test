52250ea663af0c66d75adf0901cd80c360db9d98
zhaorui <rui.999@foxmail.com>
2021-07-27 21:47:20 +0800

    day09的作业

--- day09/数据库与Excel互转/DBUtils.py
+++ day09/数据库与Excel互转/DBUtils.py
@@ -1,35 +1,36 @@
 # -*- coding:utf-8 -*-
 import pymysql
 
-host = "localhost"
-user = "root"
-passwd = "111111"
-db = "excel2db_test"
 
+class DBUtils:
+    def __init__(self, host, user, passwd, db):
+        self.host = host
+        self.user = user
+        self.passwd = passwd
+        self.db = db
 
-def select(sql, param=None, mode="all", size=1):
-    conn = pymysql.connect(host=host, user=user, passwd=passwd, db=db)
-    cur = conn.cursor()
-    cur.execute(sql, param)
-    data = None
-    if mode == "all":
+    def select(self, sql, param=None, mode="all", size=1):
+        conn = pymysql.connect(host=self.host, user=self.user, passwd=self.passwd, db=self.db)
+        cur = conn.cursor()
+        cur.execute(sql, param)
+        data = None
+        if mode == "all":
+            data = cur.fetchall()
+        elif mode == "one":
+            data = cur.fetchone()
+        elif mode == "many":
+            data = cur.fetchmany(size)
+        conn.commit()
+        cur.close()
+        conn.close()
+        return data
+
+    def show(self, sql):
+        conn = pymysql.connect(host=self.host, user=self.user, passwd=self.passwd, db=self.db)
+        cur = conn.cursor()
+        cur.execute(sql)
         data = cur.fetchall()
-    elif mode == "one":
-        data = cur.fetchone()
-    elif mode == "many":
-        data = cur.fetchmany(size)
-    conn.commit()
-    cur.close()
-    conn.close()
-    return data
-
-
-def show(sql):
-    conn = pymysql.connect(host=host, user=user, passwd=passwd, db=db)
-    cur = conn.cursor()
-    cur.execute(sql)
-    data = cur.fetchall()
-    conn.commit()
-    cur.close()
-    conn.close()
-    return data
+        conn.commit()
+        cur.close()
+        conn.close()
+        return data
